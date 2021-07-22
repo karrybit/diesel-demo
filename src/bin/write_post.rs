a4d14e8ae75aa6ecf2ebaff6b31720ea9c8b4ba3
karrybit <takumi.k.5610@gmail.com>
2021-07-22 18:54:39 +0900

    add write post

---
+++ src/bin/write_post.rs
@@ -0,0 +1,25 @@
+extern crate diesel;
+extern crate diesel_demo;
+
+use self::diesel_demo::*;
+use std::io::{stdin, Read};
+
+const EOF: &'static str = "CTRL+D";
+
+fn main() {
+    let connection = establish_connection();
+
+    println!("What would you like your title to be?");
+    let mut title = String::new();
+    stdin().read_line(&mut title).unwrap();
+    let title = &title[..(title.len() - 1)];
+    println!(
+        "\nOk! Let's write {} (Press {} when finished)\n",
+        title, EOF,
+    );
+    let mut body = String::new();
+    stdin().read_line(&mut body).unwrap();
+
+    let post = create_post(&connection, title, &body);
+    println!("\nSaved draft {} with id {}", title, post.id);
+}
